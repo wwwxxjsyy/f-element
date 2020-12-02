e4444ea347a8306069d16720f4ff6f0bc0771ca4
宋灏然 <wwwxxjsyy@163.com>
2020-12-02 17:31:44 +0800

    提交

--- packages/index.js
+++ packages/index.js
@@ -5,6 +5,7 @@ const components = [ShrButton];
 // 定义 install 方法，接收 Vue 作为参数
 const install = function (Vue) {
   // 判断是否安装，安装过就不继续往下执行
+
   if (install.installed) return;
   install.installed = true;
   // 遍历注册所有组件
