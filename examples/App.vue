77a7949fd4ecbc5b56667c433382162888e2198b
宋灏然 <wwwxxjsyy@163.com>
2020-12-10 14:02:42 +0800

    提交

--- examples/App.vue
+++ examples/App.vue
@@ -9,7 +9,7 @@
     <!-- <ShrCanvas
       @custom="handleCallback"
       :imgSrc="
-        'https://topstage.otechsolution.com:8010/other/2019/2/4/0103d38d-0ebf-437e-a7e5-6a10f5f2c4a5.jpg'
+        ''
       "
     ></ShrCanvas> -->
 
