e6d210a33045f08843912922ace9a0922ce5556b
宋灏然 <wwwxxjsyy@163.com>
2020-12-24 11:59:16 +0800

    提交

--- examples/utils/request.js
+++ examples/utils/request.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-import { Notification } from "element-ui";
+import axios from 'axios'
+import { Notification } from 'element-ui'
 // import store from "@/store";
 
 const service = axios.create({
   timeout: 3000, // request timeout
-  headers: { "Content-Type": "application/json;charset=utf-8" }
-});
+  headers: { 'Content-Type': 'application/json;charset=utf-8' }
+})
 
 // request interceptor
 service.interceptors.request.use(
   config => {
-    config.baseURL = "http://192.168.0.103:8000";
-    return config;
+    config.baseURL = 'http://192.168.10.23:8088'
+    return config
   },
   error => {
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 // response interceptor
 service.interceptors.response.use(
   response => {
-    const res = response.data;
+    const res = response.data
 
     if (res.status === 400) {
       Notification.error({
@@ -30,16 +30,26 @@ service.interceptors.response.use(
         duration: 2000
       })
 
-      return Promise.resolve(res);
+      return Promise.resolve(res)
     } else {
-      return res;
+      return res
     }
   },
   error => {
-    const err = error.response;
-    console.log("axios-err:", err)
-    return Promise.reject(err);
-  }
-);
+    const err = error.response
+    if (err.status === 404) {
+      Notification.error({
+        title: '请求内部错误',
+        message: '你的请求路径写错了',
+        duration: 2000
+      })
 
-export default service;
+      return Promise.reject(err)
+    } else {
+      console.log('axios-err:', err)
+      return Promise.reject(err)
+    }
+  }
+)
+
+export default service
