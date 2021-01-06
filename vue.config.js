e495fa3039290edea84dd5e80999ebd94e4739fc
宋灏然 <wwwxxjsyy@163.com>
2021-01-06 14:30:29 +0800

    提交

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,30 @@
-const path = require("path");
+const path = require('path')
 //去除console
 const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "./" : "/",
-  outputDir: "dist",
+  publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
+  outputDir: 'dist',
   devServer: {
     port: 9981,
     open: true,
+    hotOnly: true, //当编译失败时，不刷新页面
     overlay: {
-      warnings: false,
+      warnings: false, //用来在编译出错的时候，在浏览器页面上显示错误
       errors: true
     },
     proxy: {
-      "/s": {
-        target: "https://www.baidu.com"
+      '/s': {
+        target: 'https://www.baidu.com'
       }
+    },
+    watchOptions: {
+      // 不监听的文件或文件夹，支持正则匹配
+      ignored: /node_modules/,
+      // 监听到变化后等1s再去执行动作
+      aggregateTimeout: 1000,
+      // 默认每秒询问1000次
+      poll: 1000
     }
     // lintOnSave: false
   },
@@ -31,56 +40,57 @@ module.exports = {
             compress: {
               drop_console: true,
               drop_debugger: false,
-              pure_funcs: ['console.log'], // 移除console
-            },
-          },
-        }),
-      ],
-    },
+              pure_funcs: ['console.log'] // 移除console
+            }
+          }
+        })
+      ]
+    }
   },
   // 修改 pages 入口
   pages: {
     index: {
-      entry: "examples/main.js", // 入口
-      template: "public/index.html", // 模板
-      filename: "index.html" // 输出文件
+      entry: 'examples/main.js', // 入口
+      template: 'public/index.html', // 模板
+      filename: 'index.html' // 输出文件
     }
   },
   // 扩展 webpack 配置
   chainWebpack: config => {
     if (process.env.NODE_ENV === 'production') {
-      const CompressionPlugin = require('compression-webpack-plugin');
-      const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i;
+      const CompressionPlugin = require('compression-webpack-plugin')
+      const productionGzipExtensions = /\.(js|css|json|txt|html|ico|svg)(\?.*)?$/i
 
-      config.plugin('compressionPlugin')
-        .use(new CompressionPlugin({
+      config.plugin('compressionPlugin').use(
+        new CompressionPlugin({
           filename: '[path].gz[query]',
           algorithm: 'gzip',
           test: productionGzipExtensions,
           threshold: 10240,
           minRatio: 0.8
-        }));
+        })
+      )
     }
     // @ 默认指向 src 目录，这里要改成 examples
     // 另外也可以新增一个 ~ 指向 packages
     config.resolve.alias
-      .set("@", path.resolve("examples"))
-      .set("~", path.resolve("packages"))
-      .set("@com", path.resolve("components"))
-      .set("@src", path.resolve("src"));
+      .set('@', path.resolve('examples'))
+      .set('~', path.resolve('packages'))
+      .set('@com', path.resolve('components'))
+      .set('@src', path.resolve('src'))
 
     // 把 packages 和 examples 加入编译，因为新增的文件默认是不被 webpack 处理的
     config.module
-      .rule("js")
+      .rule('js')
       .include.add(/packages/)
       .end()
       .include.add(/examples/)
       .end()
-      .use("babel")
-      .loader("babel-loader")
+      .use('babel')
+      .loader('babel-loader')
       .tap(options => {
         // 修改它的选项...
-        return options;
-      });
+        return options
+      })
   }
-};
+}
