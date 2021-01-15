a72312d7f11067a2299cde7f25bfbdf2a251dfd6
宋灏然 <wwwxxjsyy@163.com>
2021-01-15 10:06:52 +0800

    提交

--- examples/views/About.vue
+++ examples/views/About.vue
@@ -1,5 +1,27 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <!-- <h1>This is an about page</h1> -->
+    <ShrButton @click="handlclick">测试销毁期</ShrButton>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      test: "123",
+    };
+  },
+  methods: {
+    handlclick() {
+      this.$router.push("/createcss");
+    },
+  },
+  beforeDestroy() {
+    console.log(this, "beforeDestroy");
+  },
+  destroyed() {
+    console.log(this, "destroyed");
+  },
+};
+</script>
